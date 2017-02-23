fc19cb8472d2acaf9b04897f3b0a84b1365a0391
turbo25mike <turbo25mike@gmail.com>
2017-02-22 16:26:29 -0800

    fixed uri for auth0

--- Api/AppSettings.cs
+++ Api/AppSettings.cs
@@ -1,4 +1,7 @@
-﻿namespace Api
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace Api
 {
     public interface IAppSettings {
         string DB_Connection { get; set; }
@@ -7,6 +10,12 @@
 
     public class AppSettings : IAppSettings
     {
+        public AppSettings(IConfigurationRoot config)
+        {
+            DB_Connection = Environment.GetEnvironmentVariable("APP_DB_CONNECTION") ?? config["App:DB_Connection"];
+            Auth0_Domain = Environment.GetEnvironmentVariable("AUTH0_DOMAIN") ?? config["Auth0:Domain"];
+        }
+
         public string DB_Connection { get; set; }
         public string Auth0_Domain{ get; set; }
     }
