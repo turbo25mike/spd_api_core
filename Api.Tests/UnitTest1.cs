6b42137597f34c85f6605ed5e131e48f3531a13c
turbo25mike <turbo25mike@gmail.com>
2017-02-24 15:58:34 -0800

    starting to add tests

---
+++ Api.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Api.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            false.Should().BeFalse();
+        }
+    }
+}
